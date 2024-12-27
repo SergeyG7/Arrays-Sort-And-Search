515f32bf2e275862027a13202d46df327c4ad8d6
Сергей <gvozd0770@gmail.com>
2024-12-27 18:59:54 +0200

    final

--- src/app/ArrayOperations.java
+++ src/app/ArrayOperations.java
@@ -14,6 +14,44 @@ public class ArrayOperations {
 
         System.out.println("Початковий вигляд масиву: " + Arrays.toString(array));
 
+        for (int i = 1; i < array.length; i++){
+            int key = array[i];
+            int j = i - 1;
+
+            while (j >= 0 && array[j] > key){
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = key;
+        }
+        System.out.println("Відсортований масив: " + Arrays.toString(array));
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введіть число для пошуку: ");
+        int target = scanner.nextInt();
+
+        int left = 0, right = array.length - 1;
+        int index = -1;
+        while (left <= right) {   int mid = left + (right - left) / 2;
+
+            if (array[mid] == target) {
+                index = mid;
+                break;
+            }
+
+            if (array[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        if (index != -1) {
+            System.out.println("Індекс числа " + target + " у відсортованому масиві: " + index);
+        } else {
+            System.out.println("Число " + target + " не знайдено в масиві.");
+        }
+
     }
 
 }
